8a91edabee142a1f69e9b842acb10eea4506a967
Joe McGouran <joe_mcgouran@yahoo.ie>
2011-10-04 19:06:24 +0100

    Initial Commit

---
+++ rails_projects/mh_appone/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MhAppone::Application.config.secret_token = 'da07c935fbf96394ff2855a6ac149aed2dea4f7b491b9e09068a44479e832b72e8acfc27b97529e77c7dbb4c26b23ff5f7ecef9ec887127707c25b8b6608ccc6'
